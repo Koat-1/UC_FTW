6246649da3421a07183ce913e9552012e4c56a93
zeidane218 <zeidanezidane@gmail.com>
2025-11-24 22:22:18 +0100

    modifications UC_1

--- UC_1.py
+++ UC_1.py
@@ -1,11 +1,10 @@
-def sum(array, target) :
-    for i in range(len(array)-1) :
-        if array[i] + array[i + 1] == target :
-            res = [i, i + 1]
-            break
-    return res
-
-if __name__ == "__main__":
-    print(sum([2,7,11,15], 9))      
-    print(sum([3, 2, 4], 6))         
-    print(sum([3, 3], 6))              
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = {}
+        for i, j in enumerate(nums):
+            difference = target - j
+            if difference in res :
+                return[res[difference], i]
+            res[j] = i
+   
+             
