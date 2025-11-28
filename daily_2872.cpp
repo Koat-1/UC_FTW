51063652a9dfdff401aa32446b5c98c162ea006b
Koat-1 <kessentini.youssef03@gmail.com>
2025-11-28 23:33:43 +0100

    very hard to understand and advanced topic

---
+++ daily_2872.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int dfs(int currentNode, int parentNode, vector<int> adjList[], vector<int> &nodeValues, int k, int &componenetCount)
+    {
+        int sum = 0;
+        for (auto neighborNode : adjList[currentNode])
+        {
+            if (neighborNode != parentNode)
+            {
+                sum += dfs(neighborNode, currentNode, adjList, nodeValues, k, componenetCount);
+                sum %= k;
+            }
+        }
+
+        sum += nodeValues[currentNode];
+        sum %= k;
+        if (sum == 0)
+            componenetCount++;
+
+        return sum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>> &edges, vector<int> &values, int k)
+    {
+        vector<int> adjList[n];
+        for (auto edge : edges)
+        {
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adjList[node1].push_back(node2);
+            adjList[node2].push_back(node1);
+        }
+
+        int componentCount = 0;
+
+        dfs(0, -1, adjList, values, k, componentCount);
+
+        return componentCount;
+    }
+};
